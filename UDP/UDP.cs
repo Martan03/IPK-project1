7d52ad890fd6df5ef946b9bbd4b9b97e85358be2
Martan03 <martinslezak03@gmail.com>
2024-03-25 09:20:44 +0100

    Add basic UDP implementation

--- UDP/UDP.cs
+++ UDP/UDP.cs
@@ -1,47 +1,106 @@
+using System.ComponentModel;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 public class UDP : IComm
 {
     public ComState State { get; set; }
 
-    private UdpClient Client { get; set; }
+    private UdpClient Client { get; set; } = new();
+    private IPAddress IP { get; set; }
+    private ushort Port { get; set; }
+    private ushort MsgId { get; set; } = 0;
 
-    public bool Auth(string name, string secret, string nick)
-    {
-        throw new NotImplementedException();
+
+    public UDP(string host, ushort port) {
+        IPAddress[] addresses = Dns.GetHostAddresses(host);
+        IP = addresses[0];
+        Port = port;
     }
 
-    public bool Join(string name, string channel)
-    {
-        throw new NotImplementedException();
+    public bool Auth(string name, string secret, string nick) {
+        List<byte> msg = new();
+        msg.Add((byte)Type.AUTH);
+        msg.AddRange(BitConverter.GetBytes(MsgId));
+        msg.AddRange(Encoding.UTF8.GetBytes(name));
+        msg.Add(0);
+        msg.AddRange(Encoding.UTF8.GetBytes(nick));
+        msg.Add(0);
+        msg.AddRange(Encoding.UTF8.GetBytes(secret));
+        msg.Add(0);
+        MsgId++;
+        return true;
     }
 
-    public bool Msg(string from, string msg)
-    {
-        throw new NotImplementedException();
+    public bool Join(string name, string channel) {
+        List<byte> msg = new();
+        msg.Add((byte)Type.JOIN);
+        msg.AddRange(BitConverter.GetBytes(MsgId));
+        msg.AddRange(Encoding.UTF8.GetBytes(channel));
+        msg.Add(0);
+        msg.AddRange(Encoding.UTF8.GetBytes(name));
+        msg.Add(0);
+
+        MsgId++;
+        Send(msg.ToArray());
+        return true;
     }
 
-    public void Err(string from, string msg)
-    {
-        throw new NotImplementedException();
+    public bool Msg(string from, string msg) {
+        List<byte> bytes = new();
+        bytes.Add((byte)Type.MSG);
+        bytes.AddRange(BitConverter.GetBytes(MsgId));
+        bytes.AddRange(Encoding.UTF8.GetBytes(from));
+        bytes.Add(0);
+        bytes.AddRange(Encoding.UTF8.GetBytes(msg));
+        bytes.Add(0);
+
+        MsgId++;
+        Send(bytes.ToArray());
+        return true;
     }
 
-    public void Bye()
-    {
-        throw new NotImplementedException();
+    public void Err(string from, string msg) {
+        List<byte> bytes = new();
+        bytes.Add((byte)Type.ERR);
+        bytes.AddRange(BitConverter.GetBytes(MsgId));
+        bytes.AddRange(Encoding.UTF8.GetBytes(from));
+        bytes.Add(0);
+        bytes.AddRange(Encoding.UTF8.GetBytes(msg));
+        bytes.Add(0);
+
+        MsgId++;
+        Send(bytes.ToArray());
+    }
+
+    public void Bye() {
+        List<byte> bytes = new();
+        bytes.Add((byte)Type.BYE);
+        bytes.AddRange(BitConverter.GetBytes(MsgId));
+        MsgId++;
+    }
+
+    public void Confirm(ushort id) {
+        List<byte> msg = new();
+        msg.Add((byte)Type.CONFIRM);
+        msg.AddRange(BitConverter.GetBytes(id));
+
+        Send(msg.ToArray());
     }
 
     public void Send(byte[] msg) {
-        // Client.Send(msg, msg.Length, )
+        Client.Send(msg, msg.Length, IP.ToString(), Port);
     }
 
-    public string Recv()
-    {
-        throw new NotImplementedException();
+    public string Recv() {
+        IPEndPoint? remoteEP = null;
+        byte[] recv = Client.Receive(ref remoteEP);
+
+        return Encoding.UTF8.GetString(recv);
     }
 
-    public void Close()
-    {
-        throw new NotImplementedException();
+    public void Close() {
+        Client.Close();
     }
 }
